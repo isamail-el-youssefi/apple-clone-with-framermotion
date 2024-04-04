a886be893bbfe546bd2010e54cab50fb1991ddf0
Ismail El YOUSSEFI <elyoussefiismail18@gmail.com>
2024-04-05 00:39:40 +0100

    adding the image to the hero section with the min-h-[100svh] and not 100vh to work also proply for phones

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -3,8 +3,12 @@ import { Container } from "../container";
 
 export default function Hero() {
   return (
-    <div className="bg-background text-white h-[300vh]">
-      <Container>
+    <div className="relative bg-background text-white">
+      <div className="absolute top-0 left-0 w-full">
+        <img src="/napoleon.webp" />
+      </div>
+
+      <Container className="relative z-10 min-h-[100svh]">
         <h1 className="text-5xl font-bold">
           All Apple Originals. <br />
           Only on Apple TV+.
